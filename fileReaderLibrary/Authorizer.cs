29633bc0d55b1d6360f4ba9a7014734b9dc0ab13
StevenVerwerft <steven.verwerft@student.uantwerpen.be>
2020-03-18 14:11:27 +0100

    added list of available roles to interface and simpleAuthorizer

--- fileReaderLibrary/Authorizer.cs
+++ fileReaderLibrary/Authorizer.cs
@@ -1,14 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 namespace fileReaderLibrary
 {
     public interface IAuthorizer
     {
+        List<IRole> AvailableRoles { get; set; }
         bool HasReadAccess(File file, IRole role);
     }
 
     public class SimpleAuthorizer : IAuthorizer
     {
+        public List<IRole> AvailableRoles
+        {
+            get 
+            {
+                return new List<IRole> {new Admin(), new Member()};
+            }
+            set {}
+        }
         public bool HasReadAccess(File file, IRole role)
         {
             // some dummy logic here
