e88f6d0a64967ff915821dbcb2a08a760971105a
StevenVerwerft <steven.verwerft@student.uantwerpen.be>
2020-03-19 11:52:56 +0100

    added comments

--- fileReaderUI/Program.cs
+++ fileReaderUI/Program.cs
@@ -8,15 +8,23 @@ namespace fileReaderUI
         static void Main(string[] args)
         {
 
+            // decryptor to use - can be replaced with more advanced decryptor
             IDecryptor ApplicationDecryptor = new ReverseDecryptor();
+
+            // authorizer to use - grants read access to the available roles
             IAuthorizer ApplicationAuthorizer = new SimpleAuthorizer();
+
+            // container for user input and available decryptor and authorizer
             Context ApplicationContext = new Context(ApplicationDecryptor, ApplicationAuthorizer);
+
+            // user interface - puts user input into context
+            // contains most of file validation: 
+            // (valid file - valid & matching extension - decryption supported - role security supported - access right for role)
             UI UserInterface = new UI(ApplicationContext);
     
             bool readAnotherFile = false;
             do
             {   
-                // contains meta information about file and user
                 UserInterface.AddUserInfoToContext();
 
                 // handles opening file and displaying content to the console
